58905e4389e6cfdb260d26117a13e5f66330b557
Schalk Dormehl <schalk.dormehl@gmail.com>
2022-01-12 09:34:26 +0200

    Altered contract from mintable to single issuance of 1,000,000,000 tokens directly to the ArgHe multisig

--- contracts/ArgHe.sol
+++ contracts/ArgHe.sol
@@ -5,16 +5,13 @@ import "@openzeppelin/contracts/token/ERC20/ERC20Mintable.sol";
 import "@openzeppelin/contracts/token/ERC20/ERC20Burnable.sol";
 import "@openzeppelin/contracts/GSN/Context.sol";
 
-contract ArgHe is Context, ERC20Detailed, ERC20Mintable, ERC20Burnable
+contract ArgHe is Context, ERC20Detailed, ERC20Burnable
 {
     constructor()
         public
         ERC20Detailed("Argonon Helium", "ARG", 18)
     { 
-        // Assigns minting rights to the ArgHe multisig
-        addMinter(0xD52AD7bdf3888bc0c5103Ee56693e5315033c7C3); 
-
-        // Renounces the minting rights to the contract granted by default to the initiator of this code
-        renounceMinter();
+        // Mint the only 1 billion tokens to exist, to the ArgHe multisig
+        _mint(0xD52AD7bdf3888bc0c5103Ee56693e5315033c7C3, 1000000000 * 10**18);
     }
 }
