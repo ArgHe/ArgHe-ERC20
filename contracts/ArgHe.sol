3b9b6ffb2a9ff3a401d53077516238fe0cac813b
Schalk Dormehl <schalk.dormehl@gmail.com>
2022-01-26 11:56:43 +0200

    Altered ERC20 name Added png file for indexing service reference

--- contracts/ArgHe.sol
+++ contracts/ArgHe.sol
@@ -9,7 +9,7 @@ contract ArgHe is Context, ERC20Detailed, ERC20Burnable
 {
     constructor()
         public
-        ERC20Detailed("Argonon Helium", "ARG", 18)
+        ERC20Detailed("ArgHe", "ARG", 18)
     { 
         // Mint the only 1 billion tokens to exist, to the ArgHe multisig
         _mint(0xD52AD7bdf3888bc0c5103Ee56693e5315033c7C3, 1000000000 * 10**18);
